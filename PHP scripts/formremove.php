1471f30cce3df51c13f931f6f8299b2746c63447
mayank1010 <guptamnk1010@gmail.com>
2017-07-03 05:48:52 -0700

    Script to remove Host
    This script create GUI to remove an existing host

---
+++ PHP scripts/formremove.php
@@ -0,0 +1,52 @@
+<html>
+<body>
+<!--- AIM: TO REMOVE ALREADY ADDED HOST FROM NAGIOS  -->
+<!--- SAVE THIS FILE AT LOCATION: /var/www/html/formremove.php -->
+
+<?php 
+
+	#chdir = change directory
+	chdir('/usr/local/nagios/etc/objects');
+
+	#new variable to add '.cfg' extension
+	$file_name = $_POST["hostname"].'.cfg';
+	
+	#To check wheather the host exist or not 
+	if(file_exists($file_name) != 1){
+		echo "Invalid Hostname ";
+		
+	}
+	else{
+		#To remove .cfg file of host 
+		shell_exec('rm -f ${$file_name}');
+		chdir('/usr/local/nagios/etc');
+
+		shell_exec('chmod 777 nagios.cfg');
+		$myfile = fopen("nagios.cfg","r+") or die("File not found");
+		
+		$to_match = "cfg_file=/usr/local/nagios/etc/objects/{$file_name}\n";
+	
+		fseek($myfile, 0);
+		$start = $myfile;
+		$size = 0;
+		//To itterate to the position in the file where location of $file_name.cfg is added
+		while (($line = fgets($myfile)) !== false) {
+   	     
+    		if($line == $to_match){
+    			fseek($myfile, $size);
+    			//comment that line
+    			fwrite($myfile,"#");
+    			fclose($myfile);
+    			break;
+    		}
+    		
+    		$size += strlen($line);    	
+    	}
+	
+	}
+
+?>
+<a href="http://10.129.26.133/nagios/main.php">Home Page</a>
+	
+</body>
+</html>
---
+++ PHP scripts/htmlremove.php
@@ -0,0 +1,14 @@
+<html>
+<!---AIM: FORM TO TAKE INPUT FOR HOST NAME WHICH USER WANT TO REMOVE FROM HOST -->
+<!--- SAVE THIS FILE AT LOCATION: /var/www/html/htmlremove.php-->
+	<body>
+	<!---formremove: php file to remove the host -->
+	<form action="formremove.php" method="post">
+	<br><br>
+	Name: <input type="text" name="hostname" placeholder="HostName"/>
+	<br><br>	
+		<input type="submit" value="Submit!!">
+	</form>
+	</body>
+	
+</html>

a33ce75b16e86112c54d7ddac1c09bc7739296cb
mayank1010 <guptamnk1010@gmail.com>
2017-07-04 22:06:01 +0530

    Better UI
    changed the html file for a better user interface

--- PHP scripts/htmlremove.php
+++ PHP scripts/htmlremove.php
@@ -1,14 +1,28 @@
+
 <html>
 <!---AIM: FORM TO TAKE INPUT FOR HOST NAME WHICH USER WANT TO REMOVE FROM HOST -->
 <!--- SAVE THIS FILE AT LOCATION: /var/www/html/htmlremove.php-->
+
 	<body>
 	<!---formremove: php file to remove the host -->
+	  <br><br>
+	  <center><h4> Remove a Host </h4></center> 
 	<form action="formremove.php" method="post">
-	<br><br>
-	Name: <input type="text" name="hostname" placeholder="HostName"/>
-	<br><br>	
-		<input type="submit" value="Submit!!">
+		<br><br>
+<center>
+<div style="width:230px;height:120px;border:1px solid #000;">
+
+<br>
+		<font size="2"> Name:</font>&nbsp&nbsp <input type="text" name="hostname" placeholder="HostName"  style="width:150px;height:25px;" />
+			
+		<br><br>
+		<center><input type="submit" value="Remove Host"></center>
+	</div>
+</center>
+
 	</form>
+	
 	</body>
 	
 </html>
+

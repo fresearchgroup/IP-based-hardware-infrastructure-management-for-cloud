7b1f2e8d3bbdb0f9a12511b0da254d455fdb00b1
mayank1010 <guptamnk1010@gmail.com>
2017-07-04 22:03:44 +0530

    Better UI
    modified the html file for a better user interface

--- PHP scripts/test.php
+++ PHP scripts/test.php
@@ -1,14 +1,29 @@
 <html>
-
+<!---AIM: To create form to add services to the host -->
+<!-- Save this file at location: /var/www/html/htmlservices.php-->
 	<body>
+	<!--formservices to add the service to specific host -->
+	  <br><br>
+	  <center><h4> Add a New Host </h4></center> 
 	<form action="form.php" method="post">
 		<br><br>
-		Name: <input type="text" name="name" placeholder="HostName"/>
+<center>
+<div style="width:230px;height:150px;border:1px solid #000;">
+
+<br>
+		<font size="2"> Name:</font>&nbsp&nbsp <input type="text" name="hostname" placeholder="HostName"  style="width:150px;height:25px;" />
+		<!--Dropdown menu to: Select service which you want to add-->
+			<br><br>
+			
+	<font size="2">IP:</font> <input type="text" name="ip" placeholder="IP address" style="width:150px;height:25px;" />
+				
 		<br><br>
-		Ip:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="ip" placeholder="IP address"/>
-		<br><br>
-		<input type="submit" value="Submit!!">
+		<center><input type="submit" value="Add Host"></center>
+	</div>
+</center>
+
 	</form>
+	
 	</body>
 	
 </html>
